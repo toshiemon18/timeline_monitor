4e2969dc406e67d0371e93a59337a66f287eabcf
toshiemon18 <st12318@gmail.com>
2017-08-19 05:00:23 +0900

    Add modules

---
+++ lib/twitter.rb
@@ -0,0 +1,26 @@
+# coding : utf-8
+
+require "twitter"
+
+module TimelineMonitor
+  class Twitter
+    def initialize
+      config = {
+        consumer_key: ENV["TWITTER_CONSUMER_KEY"],
+        consumer_secret: ENV["TWITTER_CONSUMER_SECRET"],
+        access_token: ENV["TWITTER_TOKEN"],
+        access_token_secret: ENV["TWITTER_TOKEN_SECRET"]
+      }
+      @client = generate_client(config)
+    end
+
+    def monitor_user
+
+    end
+
+    private
+    def generate_client(keys)
+      client = Twitter::Streaming::Client.new(keys)
+    end
+  end
+end
---
+++ lib/user_id_maneger.rb
@@ -0,0 +1,5 @@
+# coding : utf-8
+
+require "sequel"
+require "pg"
+require "logger"
