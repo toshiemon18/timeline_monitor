7d4d23226b0b82467da064fdb009287ae66bc3cc
toshiemon18 <st12318@gmail.com>
2017-08-20 15:16:22 +0900

    Implement DBDefine module

--- lib/db_define.rb
+++ lib/db_define.rb
@@ -5,12 +5,14 @@ require "pg"
 
 module TimelineMonitor
   module DBDefine
-    def check
-
-    end
-
-    def define
-
+    def define_table(db_url)
+      db = Sequel.connect(db_url, logger: Logger.new("./logs/db.log"))
+      db.create_table? :twitter_users do
+        primary_key :id
+        String      :screen_name
+        String      :uid
+      end
+      db.disconnect
     end
   end
 end
