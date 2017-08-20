b9e4593a9e2fda537bcf1e770aa64fcc1bb8cf8c
toshiemon18 <st12318@gmail.com>
2017-08-21 00:09:54 +0900

    Implement user_monitor method

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -1,9 +1,10 @@
 # coding : utf-8
 
 require "twitter"
+require "./lib/user_id_manager"
 
 module TimelineMonitor
-  class Twitter
+  class TwitterStreamer
     def initialize
       config = {
         consumer_key: ENV["TWITTER_CONSUMER_KEY"],
@@ -14,8 +15,15 @@ module TimelineMonitor
       @client = generate_client(config)
     end
 
-    def monitor_user
-
+    def monitor_user(uid_list, &block)
+      @client.filter(follow: uid_list) do |tweet|
+        tweet_info = {
+          text: tweet.text.text,
+          screen_name: tweet.user_screen_name,
+          url: tweet.url.to_s
+        }
+        yield tweet_info
+      end
     end
 
     private
