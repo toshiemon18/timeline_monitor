07172381e2e7078e8c9cb9f1ac423ea756fbac5a
Toshiaki Seino <st12318@gmail.com>
2018-01-04 01:53:58 +0900

    Fix method names

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -1,10 +1,12 @@
 # coding : utf-8
 
+require "rubygems"
 require "twitter"
 require "./lib/user_id_manager"
 
 module TimelineMonitor
   class TwitterStreamer
+    attr_accessor :client
     def initialize
       config = {
         consumer_key: ENV["TWITTER_CONSUMER_KEY"],
@@ -12,14 +14,14 @@ module TimelineMonitor
         access_token: ENV["TWITTER_TOKEN"],
         access_token_secret: ENV["TWITTER_TOKEN_SECRET"]
       }
-      @client = generate_client(config)
+      generate_client(config)
     end
 
     def monitor_user(uid_list, &block)
       @client.filter(follow: uid_list) do |tweet|
         tweet_info = {
-          text: tweet.text.text,
-          screen_name: tweet.user_screen_name,
+          text: tweet.text,
+          screen_name: tweet.user.screen_name,
           url: tweet.url.to_s
         }
         yield tweet_info
@@ -28,7 +30,12 @@ module TimelineMonitor
 
     private
     def generate_client(keys)
-      client = Twitter::Streaming::Client.new(keys)
+      @client = Twitter::Streaming::Client.new do |config|
+        config.consumer_key        ||= keys[:consumer_key]
+        config.consumer_secret     ||= keys[:consumer_secret]
+        config.access_token        ||= keys[:access_token]
+        config.access_token_secret ||= keys[:access_token_secret]
+      end
     end
   end
 end
